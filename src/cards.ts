68e689a8d7687acc80632629be412115c790aee3
Carl Thunman <thunman@gmail.com>
2023-08-05 20:21:58 -0400

    create starting deck cards

---
+++ src/cards.ts
@@ -0,0 +1,85 @@
+import { GameState, ImperiumCard } from "./types";
+
+const convincingArgumentCard: ImperiumCard = {
+  name: "Convincing Argument",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
+
+const daggerCard: ImperiumCard = {
+  name: "Dagger",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
+
+const diplomacyCard: ImperiumCard = {
+  name: "Diplomacy",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
+
+const duneTheDesertPlanetCard: ImperiumCard = {
+  name: "Dune The Desert Planet",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
+
+const reconnaissanceCard: ImperiumCard = {
+  name: "Reconnaissance",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
+
+const seekAlliesCard: ImperiumCard = {
+  name: "Seek Allies",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
+
+const signetRingCard: ImperiumCard = {
+  name: "Signet Ring",
+  destinationTypes: [],
+  agentEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  revealEffect: function (game: GameState): GameState {
+    throw new Error("Function not implemented.");
+  },
+  persuasionScore: 0
+};
